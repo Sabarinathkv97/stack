447dbb738bafec9f0e8588097f0cf52380122c70
Sabarinath K V <141605987+Sabarinathkv97@users.noreply.github.com>
2023-09-18 11:28:54 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:stack/first.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: aa(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
